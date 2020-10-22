41ede53ec98094daa321d8dd222896f7cd4576ab
Andy Chen <chenyangpoppin@hotmail.com>
2020-10-22 11:25:25 +0800

    chore: fix travis test case error

--- test/unit/webpack-base.test.js
+++ test/unit/webpack-base.test.js
@@ -4,8 +4,8 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/chen/webpack-studio/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/chen/webpack-studio/builder-webpack/test/smoke/template/src/search/index.js');
-    assert.equal(baseConfig.entry.react, '/Users/chen/webpack-studio/builder-webpack/test/smoke/template/src/react/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
+    assert.equal(baseConfig.entry.react.indexOf('builder-webpack/test/smoke/template/src/react/index.js') > -1, true);
   });
 });
