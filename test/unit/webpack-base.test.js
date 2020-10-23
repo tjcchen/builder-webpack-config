f7029903eeaf52f2263d56bb880aa577bf53f6ed
Andy Chen <chenyangpoppin@hotmail.com>
2020-10-23 09:25:31 +0800

    fix: fix builder-webpack-config issue

--- test/unit/webpack-base.test.js
+++ test/unit/webpack-base.test.js
@@ -4,8 +4,8 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
-    assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
-    assert.equal(baseConfig.entry.react.indexOf('builder-webpack/test/smoke/template/src/react/index.js') > -1, true);
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack-config/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack-config/test/smoke/template/src/search/index.js') > -1, true);
+    assert.equal(baseConfig.entry.react.indexOf('builder-webpack-config/test/smoke/template/src/react/index.js') > -1, true);
   });
 });
